d3085e6f384c17c8aef55de90c6c0a496f94e184
a17-Fesenko <elhantero@gmail.com>
2018-04-12 18:48:17 +0300

    start_work

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// handlebars middleware
+app.engine('handlebars', exphbs({ 
+    defaultLayout: 'main' 
+}));
+app.set('view engine', 'handlebars');
+
+//  index route
+app.get('/', (req, res) => {
+    const w = 'Welcome!!!';
+    res.render('index', {
+        title: w
+    });
+});
+
+// about page
+app.get('/about', (req, res) => {
+    res.send('about');
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
