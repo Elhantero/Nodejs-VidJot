4e0d175ca1e580573cf5ef1df154648dc1cedb92
a17-Fesenko <elhantero@gmail.com>
2018-04-22 20:29:33 +0300

    finish and prepare to heroku

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
 
@@ -13,6 +14,9 @@ const app = express();
 // load routes
 const ideas = require('./routes/ideas');
 const users = require('./routes/users');
+
+// Passport  config
+require('./config/passport')(passport);
 
 // map global promise - get rid of warning
 mongoose.Promise = global.Promise;
@@ -48,6 +52,9 @@ app.use(session({
     saveUninitialized: true
 }));
 
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(flash());
 
@@ -56,6 +63,7 @@ app.use( function(req, res, next) {
     res.locals.success_msg = req.flash('success_msg');
     res.locals.error_msg = req.flash('error_msg');
     res.locals.error = req.flash('error');
+    res.locals.user = req.user || null;
     next();
 });
 
--- routes/ideas.js
+++ routes/ideas.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
+const {ensureAuthenticated} = require('../helpers/auth');
+
 
 
 // Load Idea model
@@ -8,8 +10,8 @@ require('../models/Idea');
 const Idea = mongoose.model('ideas');   
 
 // Idea index page
-router.get('/', (req, res) => {
-    Idea.find({  })
+router.get('/', ensureAuthenticated, (req, res) => {
+    Idea.find({ user: req.user.id })
         .sort({ date: 'desc' })
         .then( ideas => {
             res.render('ideas/index', {
@@ -20,24 +22,29 @@ router.get('/', (req, res) => {
 
 
 //  Add Idea form
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('ideas/add');
 });
 
 // Edit idea form
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
         _id: req.params.id
     })
     .then( idea => {
-        res.render('ideas/edit', {
-            idea: idea
-        });
+        if(idea.user != req.user.id){
+            req.flash('error_msg', 'Not authirized');
+            res.redirect('/ideas');
+        } else {
+            res.render('ideas/edit', {
+                idea: idea
+            });
+        }
     });
 });
 
 // Process form
-router.post('/', (req, res) => {
+router.post('/', ensureAuthenticated, (req, res) => {
     let errors = [];
     
     if( !req.body.title ) {
@@ -56,7 +63,8 @@ router.post('/', (req, res) => {
     } else {
         const newUser = {
             title: req.body.title,
-            details: req.body.details
+            details: req.body.details,
+            user: req.user.id
         }
         
         new Idea(newUser)
@@ -69,7 +77,7 @@ router.post('/', (req, res) => {
 });
 
 // edit form process
-router.put('/:id', (req, res) => {
+router.put('/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
         _id: req.params.id
     })
@@ -87,7 +95,7 @@ router.put('/:id', (req, res) => {
 });
 
 // Delete Idea
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
     Idea.remove({ _id: req.params.id})
         .then( () => {
             req.flash('success_msg', 'Video Idea removed');
